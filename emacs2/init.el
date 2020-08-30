08124eb6027eb12efd3af19aa5be7205b8ce12cd
SkyVault <dustinneumann42@gmail.com>
2020-08-30 08:44:43 -0700

    Emacs2 electric boogaloo

---
+++ emacs2/init.el
@@ -0,0 +1,30 @@
+(require 'org)
+
+;;; Code:
+(org-babel-load-file
+ (expand-file-name "config.org"
+		   user-emacs-directory))
+
+(provide 'init)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("24714e2cb4a9d6ec1335de295966906474fdb668429549416ed8636196cb1441" default))
+ '(package-selected-packages
+   '(flycheck ccls company lsp-mode treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs evil-magit magit which-key perfect-margin automargin doom-modeline org-bullets rainbow-delimiters helm-projectile projectile dracula-theme use-package fira-code-mode evil-surround evil-leader evil-goggles)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
