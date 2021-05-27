e2567200694d803bcd49db97e31dc02c5806a6c6
dneumann <dustinneumann42@gmail.com>
2021-05-27 09:56:58 -0700

    Worked on org mode styling

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -23,7 +23,7 @@
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(helm-ag web-mode lsp-mode company flycheck evil-collection evil-magit magit projectile rainbow-delimiters helm nav-flash which-key sexy-monochrome-theme evil use-package)))
+   '(org-bullets helm-ag web-mode lsp-mode company flycheck evil-collection evil-magit magit projectile rainbow-delimiters helm nav-flash which-key sexy-monochrome-theme evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
